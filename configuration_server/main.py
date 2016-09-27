dad5f779e6d8bc7d4b5b3ae49c3cd2b41f3bfad3
DavideXX92 <davithepezz@hotmail.it>
2016-09-27 15:17:16 +0200

    Added support for GUI integration

--- configuration_server/main.py
+++ configuration_server/main.py
@@ -14,7 +14,8 @@ from server import ConfigurationServer
 logging.basicConfig(level=logging.DEBUG)
 conf_server = ConfigurationServer()
 app = falcon.API()
-app.add_route('/status/{image_id}', conf_server)
+app.add_route('/status/{vnf_type}', conf_server)
+app.add_route('/configure/{mac_vnf}/user/{user_id}', conf_server)
 logging.info("Falcon Successfully started")
 
 working_thread = Thread(target=conf_server.start_configuration_server)
--- horizon-extension/README_CODE_INFO.md
+++
@@ -1,5 +0,0 @@
-# Code info
-
-All the modified or added code is in the folder "openstack_dashboard/dashboards/project/instances/".
-The files follow the classical Django structure.
-The main code is on "openstack_dashboard/dashboards/project/instances/utils.py".
