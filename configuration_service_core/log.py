f06db91f0c0ecbe8e820218fddb44c465e566a0c
DavideXX92 <davithepezz@hotmail.it>
2017-01-26 20:32:50 +0100

    minor bugfix

---
+++ configuration_service_core/log.py
@@ -0,0 +1,16 @@
+import datetime
+
+now = datetime.datetime.now()
+
+file="/home/davide/Documenti/frog4-datastore/configuration_service/configuration_server/output.log2"
+
+def printlog(line):
+    #with open(file, "a") as myfile:
+        #myfile.write( str(now) + ": " + line)
+    try:
+        myfile = open(file, "a")
+        myfile.write( "\n" + now.strftime('%m/%d/%Y %H:%M:%S') + ": " + line)
+        myfile.flush()
+        myfile.close()
+    except IOError:
+        print("Could not open file!")
--- configuration_service_core/messaging.py
+++ configuration_service_core/messaging.py
@@ -6,6 +6,8 @@ from configuration_service_core.vnf_model import VNF
 import sys
 import json
 from configuration_service_core.my_db import get_default_configuration
+from configuration_service_core.log import print_log
+from configparser import SafeConfigParser
 import time
 
 
@@ -19,10 +21,17 @@ class MessageBus(clientSafe.ClientSafe):
         return cls._instance
 
     def __init__(self):
-        print("sto per registrarmi")
+        self.parser = SafeConfigParser()
+        self.confFile = "config/default-config.ini"
+        self.parser.read(self.confFile)
+        self.started_vnfs = []
+        self.started_vnfs_by_mac_address = {}
+        self.started_vnfs_by_id = {}
+        self.counter = 0
+        self.working_thread = None
+        self.vnf_to_configure = {}
         self.initial_registration()
         self._working_thread.join()
-        print("before sleeping")
 
     def registration(self, name, dealerurl, customer, keyfile):
         super(MessageBus, self).__init__(name=name.encode('utf8'), dealerurl=dealerurl, customer=customer.encode('utf8'), keyfile=keyfile)
@@ -32,14 +41,14 @@ class MessageBus(clientSafe.ClientSafe):
 
     def initial_registration(self):
         self.registration(name='configuration_server',
-                          dealerurl=constants.dealer,
+                          dealerurl=self.parser.get('message_broker', 'dealer'),
                           customer='public',
-                          keyfile=constants.keyfile)
+                          keyfile=self.parser.get('message_broker', 'keyfile'))
 
     def subscribe_for_tenant_association_phase(self):
-        logging.debug('trying to subscribe')
+        print_log('trying to subscribe')
         self.subscribe('tenant_association', 'noscope')
-        logging.debug('subscribed to: tenant_association')
+        print_log('subscribed to: tenant_association')
 
     def subscribe_for_default_configuration_phase(self):
         self.subscribe('default_configuration', 'noscope')
@@ -51,16 +60,16 @@ class MessageBus(clientSafe.ClientSafe):
 
     def subscribe_status_exportation(self):
         self.subscribe('status_exportation', 'noscope')
-        logging.debug('subscribed to: status exportation')
+        print_log('subscribed to: status exportation')
 
     def config(self, name, dealerURL, customer):
         super().config(name, dealerURL, customer)
 
     def on_data(self, dest, msg):
-        logging.debug(msg)
+        print_log(msg)
 
     def on_discon(self):
-        logging.debug("on_discon")
+        print_log("on_discon")
         super().on_discon()
 
     def on_pub(self, src, topic, msg):
@@ -68,13 +77,14 @@ class MessageBus(clientSafe.ClientSafe):
         src = src.decode()
         topic = topic.decode()
         msg = msg.decode()
-        logging.debug("on_pub")
-        logging.debug(src)
-        logging.debug(topic)
-        logging.debug(msg)
+        print_log("on_pub")
+        print_log(src)
+        print_log(topic)
+        print_log(msg)
         if topic == 'public.tenant_association':
             # Retrieve tenant id from the mac_address of the VM
             vnf = VNF(mac_address=src)
+            print_log("mac=" + src)
             vnf.get_detailed_info()
             self.sendmsg(src, vnf.tenant_id + ',' + vnf.name)
 
@@ -83,29 +93,29 @@ class MessageBus(clientSafe.ClientSafe):
             self.started_vnfs_by_mac_address[vnf.mac_address] = vnf
             self.started_vnfs_by_id[vnf.id] = vnf
         elif topic == 'public.default_configuration':
-            logging.debug('default configuration request received')
+            print_log('default configuration request received')
             vnf = self.started_vnfs_by_mac_address[src]
             if src in self.vnf_to_configure:
-                logging.debug('src:' + src)
+                print_log('src:' + src)
                 vnf = self.vnf_to_configure[src]
-                logging.debug(
+                print_log(
                     'publishing an old configuration for ' + src + ": " + json.dumps(vnf.configuration_to_push))
                 self.sendmsg(vnf.mac_address, json.dumps(vnf.configuration_to_push))
             else:
-                logging.debug('publishing a default configuration for: ' + src)
+                print_log('publishing a default configuration for: ' + src)
                 configuration_json = get_default_configuration(vnf.id)
                 self.sendmsg(vnf.mac_address, configuration_json)
         elif topic == 'public.status_exportation':
             # Configuration publication
             self.started_vnfs_by_mac_address[src].status = msg
-            logging.debug("Configuration json status: " + self.started_vnfs_by_mac_address[src].status + ' of vm: ' +
+            print_log("Configuration json status: " + self.started_vnfs_by_mac_address[src].status + ' of vm: ' +
                          self.started_vnfs_by_mac_address[src].name)
 
     def on_reg(self):
         '''
         Callback of the registration to the bus
         '''
-        logging.debug('registred')
+        print_log('registred')
         # Subscribe to the tenant association phase
         self.subscribe_for_tenant_association_phase()
         # Subscribe to the default configuration phase
@@ -116,12 +126,12 @@ class MessageBus(clientSafe.ClientSafe):
         self.subscribe_status_exportation()
 
     def unsubscribe(self):
-        logging.debug("unsubscribe")
+        print_log("unsubscribe")
         super().unsubscribe()
         sys.exit(0)
 
     def signal_handler(self, signal, frame):
-        logging.debug("stop")
+        print_log("stop")
         self.shutdown()
         sys.exit(0)
 
